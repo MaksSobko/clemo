bbbc94a7a4783116600bbe5c350080cd11616d73
Maksim Sobko <maxim90sobko@gmail.com>
2024-01-29 21:02:19 +0300

    fix: change html and css  of work, reviews and contacts blocks, add js

---
+++ js/form-js.js
@@ -0,0 +1,30 @@
+let formButton = document.getElementsByClassName("form-button")[0];
+let form = document.getElementsByClassName("form")[0];
+let contactsField = document.getElementsByClassName("contacts-field")[0];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  form.style.display = "none";
+
+  let formSuccess = document.createElement("div");
+  formSuccess.className = "form-success";
+  contactsField.append(formSuccess);
+
+  let formSuccessMsg = document.createElement("p");
+  formSuccessMsg.className = "form-success__msg";
+  formSuccessMsg.innerHTML = "Thank you!<br />Your message has been sent!";
+  formSuccess.append(formSuccessMsg);
+
+  let newMsgBtn = document.createElement("button");
+  newMsgBtn.className = "button form-button";
+  newMsgBtn.textContent = "new message";
+  formSuccess.append(newMsgBtn);
+
+  function newMessage() {
+    formSuccess.style.display = "none";
+    form.style.display = "block";
+  }
+
+  newMsgBtn.addEventListener("click", newMessage);
+});
